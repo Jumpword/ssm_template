50af6192bcef5cb6e7c53cca4468e6c3b7fbac88
jumping <632741793@qq.com>
2019-10-15 10:10:57 +0800

    Fix：Add Service

---
+++ src/main/java/com/ssm/service/impl/UserService.java
@@ -0,0 +1,13 @@
+package com.ssm.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author jumping-張文平
+ * @Date 2019/10/15 10:09
+ * @Version 1.0
+ */
+@Service
+public class UserService {
+    //Service层模板
+}
