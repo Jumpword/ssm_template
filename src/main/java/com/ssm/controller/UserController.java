45d2ecfc98448d00d98b8069522768bc0e11dcd6
jumping <632741793@qq.com>
2019-10-15 10:09:18 +0800

    Fix：Add Conptroller

---
+++ src/main/java/com/ssm/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.ssm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author jumping-張文平
+ * @Date 2019/10/15 10:07
+ * @Version 1.0
+ */
+
+@RestController
+@RequestMapping
+public class UserController {
+    //Controller层模板
+}
